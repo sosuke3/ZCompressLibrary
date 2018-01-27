66947a593af2888a3caa64f221afd5a99c68d76d
Josh Neta <joshneta.github@gmail.com>
2018-01-27 10:03:06 -0600

    initial commit only contians functioning decompress routine currently

---
+++ ZCompressLibrary.Tests/DecompressTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using System.IO;
+using ZCompressLibrary;
+using Xunit.Abstractions;
+
+namespace ZCompressLibrary.Tests
+{
+    public class DecompressTests
+    {
+        readonly ITestOutputHelper output;
+
+        public DecompressTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void should_decompress_file_moldorm_bin()
+        {
+            var file = File.ReadAllBytes("moldorm.bin");
+
+            var decompressed = Decompress.ALTTPDecompressGraphics(file, 0, file.Length);
+        }
+
+        [Fact]
+        public void should_decompress_file_ganon1_bin_and_be_same_as_ganon1_gfx()
+        {
+            var file = File.ReadAllBytes("ganon1.bin");
+            var expected = File.ReadAllBytes("ganontest.gfx");
+
+            var decompressed = Decompress.ALTTPDecompressGraphics(file, 0, file.Length);
+            int i = 0;
+            while(i < expected.Length && i < decompressed.Length)
+            {
+                if(expected[i] != decompressed[i])
+                {
+                    output.WriteLine($"mismatch at {i}, expected: {expected[i].ToString("X2")} decompressed: {decompressed[i].ToString("X2")}");
+                }
+                i++;
+            }
+            Assert.Equal(expected, decompressed);
+        }
+    }
+}
---
+++ ZCompressLibrary/fake_mem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZCompressLibrary
+{
+    internal static class fake_mem
+    {
+        internal static void memcpy(List<byte> dest, byte[] source, int source_offset, int length)
+        {
+            for (int i = 0; i < length; ++i)
+            {
+                dest.Add(source[source_offset + i]);
+            }
+        }
+
+        internal static void memset(List<byte> dest, byte value, int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                dest.Add(value);
+            }
+        }
+    }
+}
